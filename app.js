4219d020cc02ed9a6eee73df59e79474bbec21c1
Michelle Chou <chou.m.rdh@gmail.com>
2020-05-17 21:34:56 -0700

    created add department, add role

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2/promise");
 const cTable = require("console.table");
-const inqurier = require("inqurier");
+const inquirer = require("inquirer");
 
 const main = async () => {
     try {
@@ -15,7 +15,9 @@ const main = async () => {
         await viewDepartment(connection);
         await viewRole(connection);
         await viewEmployee(connection);
-        
+        const returnDepartment = await addDepartmentPrompt(connection);
+        await AddDepartment(connection, returnDepartment);
+        await addRolePrompt(connection)
 
         connection.end();
     } catch (err) {
@@ -25,6 +27,7 @@ const main = async () => {
 const viewDepartment = async (connection) => {
     const [rows, fields] = await connection.query("SELECT * FROM department");
     console.table(rows)
+    return rows
 };
 const viewRole = async (connection) => {
     const [rows, fields] = await connection.query("SELECT * FROM role");
@@ -36,7 +39,7 @@ const viewEmployee = async (connection) => {
     console.table(rows)
 };
 async function addDepartmentPrompt() {
-    return inqurier.prompt([
+    return inquirer.prompt([
         {
             type: "input",
             name: "departmentName",
@@ -44,17 +47,36 @@ async function addDepartmentPrompt() {
         }
     ])
 };
-const AddDepartment = async (connection) => {
-    const sqlQuery = "INSERT INTO department ?";
-    const params = [name]
+const AddDepartment = async (connection, returnDepartment) => {
+    const sqlQuery = "INSERT INTO department (name) VALUES (?)";
+    const params = [returnDepartment.departmentName]
     const [rows, fields] = await connection.query(sqlQuery, params);
-    console.table(rows);
+    console.table(`Add Department`,rows);
 };
-async function addRolePrompt(){
-    return inqurier.prompt([
+async function addRolePrompt(connection){
+    const viewDepartmentList = await viewDepartment(connection);
+    let departmentList = viewDepartmentList.map((department)=>{
+        return department.name
+    })
+    
+    return inquirer.prompt([
         {
-            
+            type:"input",
+            name:"roleTitle",
+            message:"Please enter new role title",
+        },{
+            type: "number",
+            name:"salary",
+            message:"Please enter new role salary",
+        },{
+            type:"list",
+            name:"departmentList",
+            message:"Please choose department for this role",
+            choices:departmentList
         }
     ])
+};
+const addRole = ()=>{ 
+    
 };
 main();
